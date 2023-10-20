7805598a50591c7e089e2fb23b933fa0e7ac3a9b
pkashyap639 <138331205+pkashyap639@users.noreply.github.com>
2023-10-20 18:37:29 -0400

    Point Class

---
+++ Assignment/src/Point.java
@@ -0,0 +1,21 @@
+public class Point {
+    // Variables for point x and y
+    private double x;
+    private double y;
+
+    // Parameterized constructor to set values to x and y point
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    // Get point x
+    public double getX(){
+        return x;
+    }
+
+    // Get point y
+    public double getY(){
+        return y;
+    }
+}
